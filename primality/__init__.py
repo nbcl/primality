5d48b362c2c99f13b7f9fc1c98fa054a7573f0a8
thefuzzyjew <asnyderipr@gmail.com>
2020-10-01 23:42:38 -0500

    Add Python version check (#3)
    **🎉 Squash the following commits and merge!**
    * @thefuzzyjew : Add Python version check on import
    * @thefuzzyjew : Update __init__.py

---
+++ primality/__init__.py
@@ -0,0 +1,15 @@
+import sys
+
+py_ver = sys.version_info
+if py_ver.minor < 8 or py_ver.major < 3:
+    msg = """
+    Primality requires Python >= 3.8 to run
+    Once you have Python 3.8 or greater installed you can use Primality:
+    1. Create and activate venv:
+        python3 -m venv venv
+        source venv/bin/activate
+    2. Install Primality:
+        pip3 install primality
+    More instructions at https://github.com/nbcl/primality/blob/main/docs"""
+    print(msg)
+    exit()
